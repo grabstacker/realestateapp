2189fbb2506c3670148a2e898f0bc811115ca29c
grabstacker <bradkay12@gmail.com>
2020-06-24 21:59:47 +1200

    push to github

---
+++ BookingFrm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace BookingClient
+{
+    partial class BookingFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bookBtn = new System.Windows.Forms.Button();
+            this.lstBoxBookings = new System.Windows.Forms.ListBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.emaillbl = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.phonelbl = new System.Windows.Forms.Label();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.PropertyGroupBox = new System.Windows.Forms.GroupBox();
+            this.listViewProperties = new System.Windows.Forms.ListView();
+            this.colAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colOwner = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colClient = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDateAvailable = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblBookingTime = new System.Windows.Forms.Label();
+            this.PropertyGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Location = new System.Drawing.Point(29, 45);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(181, 20);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Please Select a property";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Please select a booking slot";
+            // 
+            // bookBtn
+            // 
+            this.bookBtn.Location = new System.Drawing.Point(682, 579);
+            this.bookBtn.Name = "bookBtn";
+            this.bookBtn.Size = new System.Drawing.Size(139, 41);
+            this.bookBtn.TabIndex = 5;
+            this.bookBtn.Text = "Make Booking";
+            this.bookBtn.UseVisualStyleBackColor = true;
+            this.bookBtn.Click += new System.EventHandler(this.bookBtn_Click);
+            // 
+            // lstBoxBookings
+            // 
+            this.lstBoxBookings.FormattingEnabled = true;
+            this.lstBoxBookings.ItemHeight = 20;
+            this.lstBoxBookings.Location = new System.Drawing.Point(150, 439);
+            this.lstBoxBookings.Name = "lstBoxBookings";
+            this.lstBoxBookings.Size = new System.Drawing.Size(295, 184);
+            this.lstBoxBookings.TabIndex = 3;
+            this.lstBoxBookings.SelectedIndexChanged += new System.EventHandler(this.lstBoxBookings_SelectedIndexChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(655, 396);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(166, 26);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Location = new System.Drawing.Point(598, 399);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(51, 20);
+            this.namelbl.TabIndex = 7;
+            this.namelbl.Text = "Name";
+            // 
+            // emaillbl
+            // 
+            this.emaillbl.AutoSize = true;
+            this.emaillbl.Location = new System.Drawing.Point(598, 431);
+            this.emaillbl.Name = "emaillbl";
+            this.emaillbl.Size = new System.Drawing.Size(48, 20);
+            this.emaillbl.TabIndex = 9;
+            this.emaillbl.Text = "Email";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(655, 428);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(166, 26);
+            this.textBoxEmail.TabIndex = 8;
+            // 
+            // phonelbl
+            // 
+            this.phonelbl.AutoSize = true;
+            this.phonelbl.Location = new System.Drawing.Point(595, 463);
+            this.phonelbl.Name = "phonelbl";
+            this.phonelbl.Size = new System.Drawing.Size(55, 20);
+            this.phonelbl.TabIndex = 11;
+            this.phonelbl.Text = "Phone";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(655, 460);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(166, 26);
+            this.textBoxPhone.TabIndex = 10;
+            // 
+            // PropertyGroupBox
+            // 
+            this.PropertyGroupBox.Controls.Add(this.listViewProperties);
+            this.PropertyGroupBox.Location = new System.Drawing.Point(49, 81);
+            this.PropertyGroupBox.Name = "PropertyGroupBox";
+            this.PropertyGroupBox.Size = new System.Drawing.Size(903, 272);
+            this.PropertyGroupBox.TabIndex = 15;
+            this.PropertyGroupBox.TabStop = false;
+            this.PropertyGroupBox.Text = "Properties";
+            // 
+            // listViewProperties
+            // 
+            this.listViewProperties.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colAddress,
+            this.colOwner,
+            this.colClient,
+            this.colDateAvailable});
+            this.listViewProperties.HideSelection = false;
+            this.listViewProperties.Location = new System.Drawing.Point(36, 40);
+            this.listViewProperties.Name = "listViewProperties";
+            this.listViewProperties.Size = new System.Drawing.Size(834, 212);
+            this.listViewProperties.TabIndex = 0;
+            this.listViewProperties.UseCompatibleStateImageBehavior = false;
+            this.listViewProperties.View = System.Windows.Forms.View.Details;
+            this.listViewProperties.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // colAddress
+            // 
+            this.colAddress.Text = "Address";
+            this.colAddress.Width = 100;
+            // 
+            // colOwner
+            // 
+            this.colOwner.Text = "Owner";
+            this.colOwner.Width = 100;
+            // 
+            // colClient
+            // 
+            this.colClient.Text = "Client";
+            this.colClient.Width = 100;
+            // 
+            // colDateAvailable
+            // 
+            this.colDateAvailable.Text = "Date Available";
+            this.colDateAvailable.Width = 100;
+            // 
+            // lblBookingTime
+            // 
+            this.lblBookingTime.AutoSize = true;
+            this.lblBookingTime.Location = new System.Drawing.Point(515, 537);
+            this.lblBookingTime.Name = "lblBookingTime";
+            this.lblBookingTime.Size = new System.Drawing.Size(160, 20);
+            this.lblBookingTime.TabIndex = 16;
+            this.lblBookingTime.Text = "Current booking time:";
+            // 
+            // BookingFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 666);
+            this.Controls.Add(this.lblBookingTime);
+            this.Controls.Add(this.PropertyGroupBox);
+            this.Controls.Add(this.phonelbl);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.emaillbl);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.namelbl);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.bookBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxBookings);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "BookingFrm";
+            this.Text = "Real Estate App Booking Form";
+            this.PropertyGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bookBtn;
+        private System.Windows.Forms.ListBox lstBoxBookings;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label namelbl;
+        private System.Windows.Forms.Label emaillbl;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label phonelbl;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.GroupBox PropertyGroupBox;
+        private System.Windows.Forms.ListView listViewProperties;
+        private System.Windows.Forms.ColumnHeader colAddress;
+        private System.Windows.Forms.ColumnHeader colOwner;
+        private System.Windows.Forms.ColumnHeader colClient;
+        private System.Windows.Forms.ColumnHeader colDateAvailable;
+        private System.Windows.Forms.Label lblBookingTime;
+    }
+}
---
+++ bookingFrm.cs
@@ -0,0 +1,221 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BookingClient
+{
+    public partial class BookingFrm : Form
+    {
+        //Set all globaly used vars to interact with db data.
+        //initialise the db connection
+        public MongoClient dbClient = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false");
+        public List<string> propertyList = new List<string>();
+        public string currentBookingTime;
+        public string propertyAddress;
+        public string propertyViewingDate;
+        public int positionDelete;
+        public List<string> timeSlotArrayEdit = new List<string>();
+
+        public BookingFrm()
+        {
+            InitializeComponent();
+
+            // //initialise  the propertys from db
+            var dbList = dbClient.ListDatabases().ToList();
+            var database = dbClient.GetDatabase("Properties");
+            var collection = database.GetCollection<BsonDocument>("properties");
+            // var filter = Builders<BsonDocument>.Filter.Eq("propName", "asdfhg");
+            var documents = collection.Find(new BsonDocument()).ToList();
+
+            //run through each document found add to output list on screen
+            foreach (BsonDocument doc in documents)
+            {
+                var stringTest = doc["timeSlots"].AsBsonArray;
+
+                if (stringTest > 0)
+                {
+                    propertyList.Add(doc["propAddress"].AsString);
+                    var timestamp = doc["dateAvailable"].AsBsonDateTime.ToString();
+
+                    timestamp = timestamp.Remove(10);
+
+                    ListViewItem item = new ListViewItem(new[] { doc["propAddress"].AsString, doc["propOwner"].AsString, doc["propClient"].AsString, timestamp });
+                    listViewProperties.Items.Add(item);
+                }
+            }
+        }
+
+        //Get currently selected index and pass the property to corrsponding
+        public void lstBoxBookings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("You have selected the booking time: " + lstBoxBookings.SelectedItem.ToString() + "\n" + "Are you sure this is the time you want?", "Confirm Time Slot Booking!", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                lblBookingTime.Text = "Current booking time: " + lstBoxBookings.SelectedItem.ToString();
+
+                currentBookingTime = lstBoxBookings.SelectedItem.ToString();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+        //Check for duplicate property and date and if not submit the new property
+        //Check for input validation
+        async public void setBooking(string name, string email, string phone)
+        {
+            if (currentBookingTime != null && propertyAddress != null)
+            {
+                var database = dbClient.GetDatabase("Properties");
+                var collection = database.GetCollection<BsonDocument>("bookings");
+                var filter = Builders<BsonDocument>.Filter.Eq("propAddress", propertyAddress);
+                var filter2 = Builders<BsonDocument>.Filter.Eq("timeSlot", currentBookingTime);
+                var result = collection.Find(Builders<BsonDocument>.Filter.And(filter, filter2)).ToList();
+                if (result.Count < 1)
+                {
+                    var document = new BsonDocument
+                {
+                    {"propAddress", propertyAddress },
+                             {"buyerName", name},
+                                      {"buyerEmail",email },
+                                               {"buyerPhone",phone },
+                                                        {"dateBooked",propertyViewingDate },
+                { "timeSlot",currentBookingTime }
+                };
+                    await collection.InsertOneAsync(document);
+                    MessageBox.Show("Submitted apointment");
+                }
+                else
+                {
+                    MessageBox.Show("Im sorry that appointment has already been made!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You need to select a property and booking time!");
+            }
+        }
+
+        //handles data vailadation of the input fields and the current state being not null for propAddress and CurrentBookTime
+        public void bookBtn_Click(object sender, EventArgs e)
+        {
+            if (IsValidEmail(textBoxEmail.Text))
+            {
+                int i = 0;
+                bool result = int.TryParse(textBoxPhone.Text, out i);
+                if (result)
+                {
+                    if (textBoxName.Text == "" || textBoxEmail.Text == "" || textBoxPhone.Text == "" || propertyAddress == null || currentBookingTime == null)
+                    {
+                        MessageBox.Show("You forgot a field!");
+                    }
+                    else
+                    {
+                        DialogResult dialogResult = MessageBox.Show("Your Booking is as follows: \n" + "Property Address: " + propertyAddress + "\n" + "Booking Time: " + currentBookingTime + "\n" + "Your Name: " + textBoxName.Text + "\n" + " Email: " + textBoxEmail.Text + "\n" + "Contact Phone: " + textBoxPhone.Text + "\n" + "Are these details correct?", "Confirm Time Slot Booking!", MessageBoxButtons.YesNo);
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            //Reset all vars used in the booking for lables and state
+                            lblBookingTime.Text = "Current booking time: " + lstBoxBookings.SelectedItem.ToString();
+
+                            currentBookingTime = lstBoxBookings.SelectedItem.ToString();
+                            setBooking(textBoxName.Text, textBoxEmail.Text, textBoxPhone.Text);
+                            positionDelete = lstBoxBookings.SelectedIndex;
+                            removeBookingSlot(propertyAddress, currentBookingTime);
+                            currentBookingTime = null;
+                            propertyAddress = null;
+                            propertyViewingDate = null;
+                            lstBoxBookings.Items.Clear();
+                            lblBookingTime.Text = "Current booking time: ";
+                        }
+                        else if (dialogResult == DialogResult.No)
+                        {
+                            //do something else
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Not a vaild Phone Number!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("The email address is not vaild!");
+            }
+        }
+
+        //checks for an exsisting property address and updates the timeSlotsArray
+        public void removeBookingSlot(string propertyaddress, string currentbookingtime)
+        {
+            var database = dbClient.GetDatabase("Properties");
+            var collection = database.GetCollection<BsonDocument>("properties");
+            var filter = new BsonDocument("propAddress", propertyaddress);
+            timeSlotArrayEdit.RemoveAt(positionDelete);
+            var update = Builders<BsonDocument>.Update.Set("timeSlots", timeSlotArrayEdit);
+            var document = collection.UpdateOne(filter, update);
+        }
+
+        //Checks a vaild email is being attemptedd
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //Gets the lists view index and property view date available
+        //corrosponds that position from the index to the position in the array of properties propList
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listViewProperties.Enabled = false;
+            lstBoxBookings.Items.Clear();
+            lblBookingTime.Text = "Current booking time: ";
+            currentBookingTime = null;
+            propertyAddress = null;
+
+            if (listViewProperties.SelectedItems.Count > 0)
+            {
+                propertyAddress = listViewProperties.SelectedItems[0].Text;
+                propertyViewingDate = listViewProperties.SelectedItems[0].SubItems[3].Text;
+
+                var database = dbClient.GetDatabase("Properties");
+                var collection = database.GetCollection<BsonDocument>("properties");
+                var filter = new BsonDocument("propAddress", propertyList[listViewProperties.Items.IndexOf(listViewProperties.SelectedItems[0])]);
+                var documents = collection.Find(filter).ToList();
+
+                //finds the corrosponding document in the db and pushes the time slots for that document to the lstBoXBookings Items
+                foreach (BsonDocument doc in documents)
+                {
+                    if (doc["timeSlots"].AsBsonArray.Count > 0)
+                    {
+                        var timeSlotArray = doc["timeSlots"].AsBsonArray.ToArray();
+                        lstBoxBookings.Items.Clear();
+                        timeSlotArrayEdit.Clear();
+                        foreach (var slot in timeSlotArray)
+                        {
+                            lstBoxBookings.Items.Add(slot);
+                            timeSlotArrayEdit.Add(slot.AsString);
+                        }
+                    }
+                    else
+                    {
+                        timeSlotArrayEdit.Clear();
+                        timeSlotArrayEdit.Clear();
+                        MessageBox.Show("Sorry no more appointments left!");
+                    }
+                }
+            }
+            listViewProperties.Enabled = true;
+        }
+    }
+}
